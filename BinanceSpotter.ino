8a41427c3f2109e7c5cdfeb04968c4634b3e21c1
TinyAxolotl <metaleastua@gmail.com>
2024-11-12 01:51:47 +0100

    Add OTA update
    Update new firmware and drop files to SPIFFS via ESP-IP/update/

--- BinanceSpotter.ino
+++ BinanceSpotter.ino
@@ -1,5 +1,9 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <WebServer.h>
+#include <ElegantOTA.h>
+
+WebServer server(80);
 
 const char* ssid = "wifiname";
 const char* password = "pass";
@@ -7,11 +11,11 @@ const char* password = "pass";
 void setup() {
   Serial.begin(115200);
   Serial.println("Binance spotter started");
-  
+
   WiFi.disconnect(true);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  
+
   Serial.print("WiFi connecting");
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -21,8 +25,12 @@ void setup() {
 
   Serial.printf("\nWifi connected!\n");
   Serial.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+
+  ElegantOTA.begin(&server);
+  server.begin();
 }
 
 void loop() {
-
+  server.handleClient();
+  ElegantOTA.loop();
 }
